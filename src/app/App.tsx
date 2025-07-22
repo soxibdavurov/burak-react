abd5088a068dfbafbc42f36dc6bd52ef0ed9e827
soxibdavurov <soxib.davurov@gmail.com>
2025-07-22 23:49:20 +0900

    feat: create routing system

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,48 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Link, Route, Switch } from "react-router-dom";
+import { About } from "./screens/About";
+import { Users } from "./screens/Users";
 
 function App() {
-  return <Container sx={{background: "orange"}}>
-    <Stack flexDirection={"column"}>
-      <Box sx={{my: 4}}>
-        <Typography variant="h4" component={"h4"}>
-          Create React app
-        </Typography>
-      </Box>
-      <Box>
-        <RippleBadge badgeContent={4}>
+  return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <Button variant="contained">Contained</Button>   
-      
-      </RippleBadge>
-      </Box>
-    </Stack>
-  </Container>;
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+  );
 }
+
+function Home() {
+  return <Container>Home</Container>;
+}
+
 
 export default App;
 
