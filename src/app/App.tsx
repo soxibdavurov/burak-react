d2249a07ab69040035af0d56d5e2521bea617383
soxibdavurov <soxib.davurov@gmail.com>
2025-07-22 16:04:52 +0900

    fix: MUI customized inegration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-  return <Container maxWidth="sm">
+  return <Container sx={{background: "orange"}}>
     <Stack flexDirection={"column"}>
       <Box sx={{my: 4}}>
         <Typography variant="h4" component={"h4"}>
           Create React app
         </Typography>
       </Box>
-      <Button variant="contained">Contained</Button>
+      <Box>
+        <RippleBadge badgeContent={4}>
+
+      <Button variant="contained">Contained</Button>   
+      
+      </RippleBadge>
+      </Box>
     </Stack>
   </Container>;
 }
