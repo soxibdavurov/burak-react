fdf14bb7e8213af0b5c07ede7c18e2dd8007f63d
soxibdavurov <soxib.davurov@gmail.com>
2025-07-25 01:37:17 +0900

    fix: header and footer part one

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,3 +1,59 @@
+import { Container, Stack, Box, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function HomeNavbar() {
-    return <div>Home Navbar</div>;
+    const authMember = null;
+    return (
+    
+    <div className="home-navbar">
+        <Container sx={{mt: "55px", height: "642px"}}>
+            <Stack 
+            sx={{height: "50px"}} 
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            >
+                <Box>
+                    <NavLink to="/">
+                        <img style={{width: "125px", height:"30px"}} src="/icons/burak.svg" />
+                    </NavLink>
+                </Box>
+            <Stack flexDirection={"row"} 
+            justifyContent={"space-between"}
+            minWidth={"700px"}
+            alignItems={"center"}
+            >
+                 <Box className={"hover-line"}>
+                    <NavLink activeClassName={"underline"} to="/">Home  </NavLink>
+                </Box>
+                <Box className={"hover-line"}>
+                    <NavLink activeClassName={"underline"} to="/products">Products</NavLink>
+                </Box>
+                {authMember ? (
+                 <Box className={"hover-line"}>
+                    <NavLink  activeClassName={"underline"} to="/orders">Orders</NavLink>
+                </Box>
+                ): null}
+                {authMember ? (
+                 <Box className={"hover-line"}>
+                    <NavLink activeClassName={"underline"} to="/member-page">My page</NavLink>
+                </Box>
+                ): null}
+                 <Box className={"hover-line"}>
+                    <NavLink activeClassName={"underline"} to="/help">Help</NavLink>
+                </Box>
+                {/* */}
+
+                {!authMember ? (<Box>
+                    <Button variant="contained" style={{background: "#3776CC", 
+                        color:"#f8f8ff"}}>
+                        Login</Button></Box>) : (
+                    <img />
+                ) }
+            </Stack>
+            </Stack>
+            <Stack>Detail</Stack>
+        </Container>
+    </div>
+    );
 }
