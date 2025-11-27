c0dd50e592814d2f8d6b3aa1d61c65d36ace7ed7
soxibdavurov <soxib.davurov@gmail.com>
2025-11-28 02:00:14 +0900

    continue ...

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,34 @@
+/** REACT APP STATE */
+
+import { Member } from "./member";
+import { Order } from "./order";
+import { Product } from "./product";
+
+export interface AppRootState {
+    homePage: HomePageState;
+    productsPage: ProductsPageState;
+    ordersPage: OrdersPageState;
+}
+
+/* HOMEPAGE */
+
+export interface HomePageState{
+    popularDishes: Product[];
+    newDishes: Product[];
+    topUsers: Member[];
+}
+
+/* PRODUCTS PAGE */
+export interface ProductsPageState{
+    restaurant: Member | null;
+    chosenProduct: Product | null;
+    products: Product[];
+}
+
+
+/* ORDERS PAGE */
+export interface OrdersPageState {
+    pausedOrders: Order[];
+    processOrders: Order[];
+    finishedOrders: Order[];
+} 
