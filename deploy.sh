22bbe5f501c57b05b47370333584d92ad2de695e
soxibdavurov <soxib.davurov@gmail.com>
2025-11-29 20:25:52 +0900

    minor change

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,8 @@
 
 #PRODUCTION
 git reset --hard
-<<<<<<< HEAD
 git checkout main
 git pull origin main
-=======
-git checkout master
-git pull origin master
->>>>>>> develop
 
 npm i yarn -g
 yarn global add serve
