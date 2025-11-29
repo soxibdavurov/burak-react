b225ee2d4fd322364ed35aaf925e090e5fd57197
soxibdavurov <soxib.davurov@gmail.com>
2025-11-29 23:16:11 +0900

    feat: prepare to deploy

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,3 @@ yarn global add serve
 yarn
 yarn run build
 pm2 start "yarn run start:prod" --name=BURAK-REACT
-
-# #DEVELOPMENT
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# npm i
-# pm2 start "npm run start:dev" --name=BURAK
