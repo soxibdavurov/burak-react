c3e38ebd5d03d821cad9c0516a7253590a606f5f
soxibdavurov <soxib.davurov@gmail.com>
2025-11-30 19:54:41 +0900

    fix: modify deploy process and ignore dotenv files

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-#PRODUCTION
-git reset --hard
-git checkout master
-git pull origin master
+set -e  # biror buyruq yiqilsa, script to'xtasin
 
-npm i yarn -g
-yarn global add serve
-yarn
+# PRODUCTION (kerak bo'lsa ochib ishlatasan)
+# git reset --hard
+# git checkout master
+# git pull origin master
+
+# Yarn / dependencylar (bir marta o'rnatib qo'ygansan bo'lsa, quyidagilarni komment qilsang ham bo'ladi)
+# npm install -g yarn
+# yarn global add serve
+
+# Loyihaning dependencylarini o'rnatish
+yarn install
+
+# Production build
 yarn run build
-pm2 start "yarn run start:prod" --name=BURAK-REACT
+
+# Eski pm2 process bo'lsa o'chiramiz (bo'lmasa ham xato bermasin)
+pm2 delete BURAK-REACT 2>/dev/null || true
+
+# pm2 orqali yarn scriptni ishga tushirish
+pm2 start yarn --name BURAK-REACT -- run start:prod
+
+# pm2 configni saqlab qo'yish (server qayta yoqilganda ham ishlashi uchun, agar pm2 startup qilgan bo'lsang)
+pm2 save
